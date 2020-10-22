77e950f60567a19d435f235befe12b13bb68bc2d
Svetlana-Orlova <Svetlana-Orlova@github.com>
2020-10-22 21:39:54 +0300

    Четвертое задание

--- js/setup.js
+++ js/setup.js
@@ -4,10 +4,19 @@ var wizardNames = ['Иван', 'Хуан Себастьян', 'Мария', 'К�
 var wizardSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var wizardCoatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var wizardEyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var wizardFireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var quantity = 4;
 var modalSetup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = modalSetup.querySelector('.setup-close');
 var listWizards = document.querySelector('.setup-similar-list');
 var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var playerWizardCoat = document.querySelector('.setup-wizard').querySelector('.wizard-coat');
+var playerWizardEyes = document.querySelector('.setup-wizard').querySelector('.wizard-eyes');
+var playerWizardFireball = document.querySelector('.setup-fireball-wrap');
+var eyesColor = document.querySelector('.eyes-color');
+var coatColor = document.querySelector('.coat-color');
+var fireballColor = document.querySelector('.fireball-color');
 
 var getRandomElement = function (arr) {
   var randomElement = arr[Math.floor(Math.random() * arr.length)];
@@ -46,5 +55,64 @@ for (var i = 0; i < quantity; i++) {
 }
 
 listWizards.appendChild(fragment);
+
+var onPopupEscPress = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  modalSetup.classList.remove('hidden');
+
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  modalSetup.classList.add('hidden');
+
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    closePopup();
+  }
+});
+
+playerWizardCoat.addEventListener('click', function () {
+  var randomColor = getRandomElement(wizardCoatColors);
+  playerWizardCoat.style.fill = randomColor;
+  coatColor.value = randomColor;
+});
+
+playerWizardEyes.addEventListener('click', function () {
+  var randomColor = getRandomElement(wizardEyesColors);
+  playerWizardEyes.style.fill = randomColor;
+  eyesColor.value = randomColor;
+});
+
+playerWizardFireball.addEventListener('click', function () {
+  var randomColor = getRandomElement(wizardFireballColors);
+  playerWizardFireball.style.background = randomColor;
+  fireballColor.value = randomColor;
+});
+
+
 document.querySelector('.setup-similar').classList.remove('hidden');
 modalSetup.classList.remove('hidden');
